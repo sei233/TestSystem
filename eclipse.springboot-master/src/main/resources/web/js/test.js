ab8c72ed2fdcb0ceee54becb2b0f44043188d87d
石乾 <1136420833@qq.com>
2018-03-05 15:49:27 +0800

    提交内容：全新的仓库 提交时间：2018年3月2日14:06:59 提交人：石乾

--- eclipse.springboot-master/src/main/resources/web/js/test.js
+++ eclipse.springboot-master/src/main/resources/web/js/test.js
@@ -2,7 +2,7 @@ $(function () {
     var names="shiqian";
     $('#delete1').click(function () {
          $("input[name='test']:checkbox:checked").each(function () {   //遍历所有选中的checkbox
-             names = $(this).parent("td").val();
+             names = $(this).parent("td");
             $("#table_id_example").append(names);
         });
      });
