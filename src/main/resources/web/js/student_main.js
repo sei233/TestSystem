08e97433a6ab22be6f4d5191b7069502287c495d
石乾 <1136420833@qq.com>
2018-03-12 09:43:59 +0800

    提交内容：学生界面跳转补充 提交时间：2018年3月12日9:42:59 提交人：石乾

--- src/main/resources/web/js/student_main.js
+++ src/main/resources/web/js/student_main.js
@@ -37,8 +37,8 @@ $(function () {
                     data: "",
                     dataType: "json",
                     success: function (data) {
-                        if(data.entrance==0) window.location.assign("../study/study_test.html")
-                        if(data.entrance==1) window.location.assign("../study/study_repo.html")
+                        if(data.study==0) window.location.assign("../study/study_test.html")
+                        if(data.study==1) window.location.assign("../study/study_repo.html")
                         alert(data.errorCode + "   " + data.errorMessage);
                     },
                     error: function (data) {
@@ -53,8 +53,8 @@ $(function () {
                     data: "",
                     dataType: "json",
                     success: function (data) {
-                        if(data.entrance==0) window.location.assign("../graduation/gradu_test.html")
-                        if(data.entrance==1) window.location.assign("../graduation/gradu_repo.html")
+                        if(data.graduation==0) window.location.assign("../graduation/gradu_test.html")
+                        if(data.graduation==1) window.location.assign("../graduation/gradu_repo.html")
                         alert(data.errorCode + "   " + data.errorMessage);
                     },
                     error: function (data) {
