8608a8b63607a71ccd501eec6a84f922e0ce9d6c
石乾 <1136420833@qq.com>
2018-03-19 09:43:07 +0800

    提交内容：教师主界面 提交时间：2018年3月19日9:41:59 提交人：石乾

---
+++ src/main/resources/web/js/teacher_main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $.post({
+        url: 'http://localhost:8080/user/name',
+        data: "",
+        dataType: "json",
+        success: function(data) {
+            function usersList(data){
+                var usersList=data.usersList;
+                $.each(usersList,function (index,obj) {
+                    $("#name").append("<label>" + obj.userName + "</label>");
+                    $("#phone").append("<label>" + obj.userPhone + "</label>");
+                    $("#role").append("<label>" + obj.userRole + "</label>");
+                });
+            }
+            usersList(data);
+
+            $('#teacher_entrance').click(function (){
+                window.location.assign("../entrance/entrance_search.html")
+            });
+
+        },
+        error:function (data) {
+            alert(data.responseJSON.errorCode + "   " + data.responseJSON.errorMessage);
+        }
+    });
+});
